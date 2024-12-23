97ae796929fd2992d8eef7d72d70c649e9a4580e
LuuuXXX <luuux98@163.com>
2024-12-23 16:08:50 +0800

    rebase master

--- tests/testsuite/rim_dev/vendor/mod.rs
+++ tests/testsuite/rim_dev/vendor/mod.rs
@@ -19,14 +19,10 @@ fn case() {
         .stdout_eq(file!["stdout.log"])
         .stderr_eq(file!["stderr.log"]);
 
-    let dir_to_walk = test_home.join("vd/1010-01-01");
+    let dir_to_walk = test_home.join("packages").join("vd").join("1010-01-01");
     let entries = utils::walk_dir(&dir_to_walk, true).unwrap();
 
-    let expected = vec![
-        dir_to_walk.join("a.tar.xz"),
-        dir_to_walk.join("b.tar.xz"),
-        dir_to_walk.join("c.tar.xz"),
-    ];
+    let expected = vec![dir_to_walk.join("a.tar.xz"), dir_to_walk.join("b.tar.xz")];
 
     for exp in expected {
         assert!(entries.contains(&exp));
