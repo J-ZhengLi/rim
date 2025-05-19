2e0485dcea7ba6d4e2dc9c45510f0ffd7be0ca1c
J-ZhengLi <lizheng135@huawei.com>
2025-05-19 17:41:57 +0800

    fix failing tests & remove empty `stderr.log`s

--- tests/testsuite/rim_cli/installer/help/mod.rs
+++ tests/testsuite/rim_cli/installer/help/mod.rs
@@ -1,15 +1,13 @@
+use super::INSTALLER_PROCESS;
 use rim_test_support::file;
 use rim_test_support::prelude::*;
-use rim_test_support::project::ProjectBuilder;
 
 #[rim_test]
 fn case() {
-    let test_process = ProjectBuilder::installer_process();
-    let project = test_process.build();
-    project
+    INSTALLER_PROCESS
+        .command()
         .arg("--help")
         .assert()
         .success()
-        .stdout_eq(file!["stdout.log"])
-        .stderr_eq(file!["stderr.log"]);
+        .stdout_eq(file!["stdout.log"]);
 }
--- tests/testsuite/rim_cli/installer/mod.rs
+++ tests/testsuite/rim_cli/installer/mod.rs
@@ -1,2 +1,8 @@
+use rim_test_support::project::ProjectBuilder;
+use std::sync::LazyLock;
+
 mod help;
 mod run;
+
+static INSTALLER_PROCESS: LazyLock<ProjectBuilder> =
+    LazyLock::new(|| ProjectBuilder::installer_process());
