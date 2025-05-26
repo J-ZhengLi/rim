dd1d121e3fe36d6b7acd64937984aa5a38e15f81
LuuuXXX1 <luuux98@163.com>
2025-05-26 16:01:38 +0800

    only gui

--- ci/run.sh
+++ ci/run.sh
@@ -7,12 +7,14 @@ export MSYS_NO_PATHCONV=1
 script=`cd $(dirname $0) && pwd`/`basename $0`
 
 # 检查环境变量 IMAGE 是否存在
-if [ -z "$IMAGE" ]; then
-  echo "Error: IMAGE environment variable is not set."
+if [ $# -eq 0 ]; then
+  echo "Error: Please specify the IMAGE as the first argument."
+  echo "Usage: $0 <IMAGE>"
   exit 1
 fi
-image="$IMAGE"
-echo Current image: $image
+
+image="$1"
+echo "Current image: $image"
 
 # MacOS reports "arm64" while Linux reports "aarch64". Commonize this.
 machine="$(uname -m | sed 's/arm64/aarch64/')"
@@ -24,6 +26,7 @@ root_dir="`dirname $ci_dir`"
 
 if [ -f "$docker_dir/$image/Dockerfile" ]; then
     dockerfile="$docker_dir/$image/Dockerfile"
+    context="$script_dir"
     echo "::group::Building docker image for $image"
 
     # Print docker version
@@ -37,20 +40,24 @@ if [ -f "$docker_dir/$image/Dockerfile" ]; then
         "-f" "$dockerfile"
         "$context"
     )
+
+    docker buildx "${build_args[@]}" --output=type=docker
 else
     echo Invalid image: $image
     exit 1
 fi
 
-args="$args --volume $root_dir:/checkout"
-command=(/checkout/ci/run.sh)
+# command=(/checkout/ci/scripts/command.sh)
+command=(/scripts/command.sh)
 
 docker \
   run \
-  --workdir /checkout/obj \
-  $args \
-  --env CI_JOB_NAME="${CI_JOB_NAME-$IMAGE}" \
+  --workdir /checkout/rim \
+  --env $EDITION \
+  --volume $root_dir:/checkout \
   --init \
   --rm \
   rim \
   "${command[@]}"
+
+ls -al
